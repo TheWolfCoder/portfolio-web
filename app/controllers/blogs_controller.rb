78bbff245c65d43c0ff0de83f04d8b39e94929f8
Stephan Brown <stephan.r.brown@gmail.com>
2017-03-25 07:18:58 +0000

    implemented partials for blog, font awesome and pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.special_blogs
+    @blogs = Blog.page(params[:page]).per(5)
   #  binding.pry
     @page_title = "#{current_user.first_name}'s Portfolio Blog"
   end
