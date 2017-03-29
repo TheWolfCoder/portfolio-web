88a5b3832243342d6f0af916eb8e91d9263eccf0
Stephan Brown <stephan.r.brown@gmail.com>
2017-03-29 06:59:25 +0000

    Installed jquery ui, sortable html5 library and css calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery-ui
 //= require jquery_ujs
+//= require html.sortable
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require bootstrap
