3c40c6c50b914f3926d8cfa3bd08b251b772c26d
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-03 10:35:08 +0000

    Implemented action cable for comments

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( blogs.css )
 Rails.application.config.assets.precompile += %w( portfolios.css )
+Rails.application.config.assets.precompile += %w( cable.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
