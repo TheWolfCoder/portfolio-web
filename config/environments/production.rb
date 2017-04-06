565916fc39b24d5f29ea08a95cad4b0d303a10c1
Stephan Brown <stephan.r.brown@gmail.com>
2017-04-06 06:57:16 +0000

    Integrated porduction calls for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://shielded-eyrie-58623.herokuapp.com', 'http://shielded-eyrie-58623.herokuapp.com']
+  config.action_cable.url = "wss://shielded-eyrie-58623.herokuapp.com/cable"
 end
